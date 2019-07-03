83456a9ca0e1367be5d437d8c05e56903d537b65
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-03 15:05:39 +0100

    stopping the dl of the data

--- processing/runner.sh
+++ processing/runner.sh
@@ -7,26 +7,26 @@
 # $5 - Longitude of outlet
 
 # Set up paths so we have a folder for each sub zone
-cd /data/Geog-c2s2/zanardo/
-mkdir $1
-cd $1
-
-# Download the tiles we need
-python /data/home/faw513/zanardo-replication/processing/get_urls.py $1.shp | xargs -n 1 -P 8 wget -nv
-
-# Build virtual raster from tiles
-gdalbuildvrt input.vrt *.hgt
-
-# Clip the merged raster using the corresponding shapefile
-gdalwarp -multi -wo 'NUM_THREADS=val/ALL_CPUS' -srcnodata -32768 -dstnodata -9999 -of ENVI input.vrt tmp.bil
-
-# Reproject the clipped raster to utm and save as a floating point file
-gdalwarp -t_srs '+proj=utm +zone='$2' +datum=WGS84 +'$3'' -of ENVI -ot Float32 tmp.bil $1.bil
-
-# Tidy up some temp files
-rm tmp.*
-rm *.hgt
-rm *.vrt
+# cd /data/Geog-c2s2/zanardo/
+# mkdir $1
+# cd $1
+#
+# # Download the tiles we need
+# python /data/home/faw513/zanardo-replication/processing/get_urls.py $1.shp | xargs -n 1 -P 8 wget -nv
+#
+# # Build virtual raster from tiles
+# gdalbuildvrt input.vrt *.hgt
+#
+# # Clip the merged raster using the corresponding shapefile
+# gdalwarp -multi -wo 'NUM_THREADS=val/ALL_CPUS' -srcnodata -32768 -dstnodata -9999 -of ENVI input.vrt tmp.bil
+#
+# # Reproject the clipped raster to utm and save as a floating point file
+# gdalwarp -t_srs '+proj=utm +zone='$2' +datum=WGS84 +'$3'' -of ENVI -ot Float32 tmp.bil $1.bil
+#
+# # Tidy up some temp files
+# rm tmp.*
+# rm *.hgt
+# rm *.vrt
 
 # Run the LSD code
 cd /data/home/faw513/LSDTopoTools2/src/lsdtt-drivers/
