8e958ac65d4dce731800eb9b38c0d5f91b9c491e
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-08 16:27:47 +0100

    removing a loop that was breaking things when running in parallel.

--- processing/snapper.py
+++ processing/snapper.py
@@ -5,36 +5,36 @@ from fiona.crs import from_epsg
 from glob import glob
 import csv
 import sys
-print('passed in arg:', sys.argv[1])
+
 points = fiona.open('/data/home/faw513/zanardo-replication/shapefiles/pts_wgs84.shp')
 # points = fiona.open('../shapefiles/pts_wgs84.shp')
 
-for i, p in enumerate(points[0]['geometry']['coordinates']):
-    print(i,p)
-    point = Point(p)
-    buffered = point.buffer(0.5)
+index = int(sys.argv[1].split('_')[1])
 
-    channel_pts = []
-    NI = []
+point = Point(points[0]['geometry']['coordinates'][index])
+buffered = point.buffer(0.05)
 
-    filename = '/data/Geog-c2s2/zanardo/{}/full_network_{}.csv'.format(sys.argv[1], sys.argv[1])
-    print('built filename:', filename)
-    with open(filename) as csvfile:
-    # with open('full_network_buffer_{}.csv'.format(i)) as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader, None)
-        for row in reader:
-            if float(row[3]) > 5:
+channel_pts = []
+NI = []
 
-                tmpPoint = Point(float(row[1]), float(row[0]))
+filename = '/data/Geog-c2s2/zanardo/{}/full_network_{}.csv'.format(sys.argv[1], sys.argv[1])
+print('built filename:', filename)
+with open(filename) as csvfile:
+# with open('full_network_buffer_{}.csv'.format(i)) as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    for row in reader:
+        if float(row[3]) > 5:
 
-                if tmpPoint.within(buffered):
+            tmpPoint = Point(float(row[1]), float(row[0]))
 
-                    NI.append(int(row[4]))
-                    channel_pts.append(Point(tmpPoint))
+            if tmpPoint.within(buffered):
 
-    nearest_geoms = nearest_points(point, MultiPoint(channel_pts))
+                NI.append(int(row[4]))
+                channel_pts.append(Point(tmpPoint))
 
-    for i, p in enumerate(channel_pts):
-        if p == nearest_geoms[1]:
-            print(NI[i])
+nearest_geoms = nearest_points(point, MultiPoint(channel_pts))
+
+for i, p in enumerate(channel_pts):
+    if p == nearest_geoms[1]:
+        print(NI[i])
