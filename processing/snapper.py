3d5cfd0a799b53ed2e1279eb03dc10de89a2bfcd
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-07-08 16:21:07 +0100

    debugging the shapely code on the hpc

--- processing/snapper.py
+++ processing/snapper.py
@@ -5,17 +5,22 @@ from fiona.crs import from_epsg
 from glob import glob
 import csv
 import sys
-
+print('passed in arg:', sys.argv[1])
 points = fiona.open('/data/home/faw513/zanardo-replication/shapefiles/pts_wgs84.shp')
+# points = fiona.open('../shapefiles/pts_wgs84.shp')
 
 for i, p in enumerate(points[0]['geometry']['coordinates']):
+    print(i,p)
     point = Point(p)
-    buffered = point.buffer(0.05)
+    buffered = point.buffer(0.5)
 
     channel_pts = []
     NI = []
 
-    with open('/data/Geog-c2s2/zanardo/{}/full_network_{}.csv'.format(sys.argv[1], sys.argv[1])) as csvfile:
+    filename = '/data/Geog-c2s2/zanardo/{}/full_network_{}.csv'.format(sys.argv[1], sys.argv[1])
+    print('built filename:', filename)
+    with open(filename) as csvfile:
+    # with open('full_network_buffer_{}.csv'.format(i)) as csvfile:
         reader = csv.reader(csvfile)
         next(reader, None)
         for row in reader:
